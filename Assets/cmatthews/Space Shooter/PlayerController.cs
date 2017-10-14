fce1e4c717dd3564ee439e8577f68a25295c6d2a
Chris Matthews <Chris Matthews>
2017-10-13 19:43:05 -0500

    started working on Galaga clone

---
+++ Assets/cmatthews/Space Shooter/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody body;
+
+    public float speed;
+    
+
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+
+        transform.position += Vector3.right * x * Time.deltaTime * speed;
+	}
+}
